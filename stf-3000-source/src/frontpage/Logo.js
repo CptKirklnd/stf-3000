4af41b2e4edd623a977bc4966fd7d3ee50feaadf
Campbell Parker <CptKirklnd@users.noreply.github.com>
2022-11-14 23:24:13 +0000

    I broke it I think

---
+++ stf-3000-source/src/frontpage/Logo.js
@@ -0,0 +1,16 @@
+/** Copyright (c) 2022, Cam Parker **/
+
+import {
+  Image
+} from '@chakra-ui/react';
+import React from 'react';
+
+function Logo(){
+  <Image // TODO: put an actual header logo here
+    src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png"
+    alt="Strangetown Funk"
+    
+  />
+}
+
+export default Logo;
